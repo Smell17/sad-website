ef5331d531062b56486bb7747e8b6e7f09618568
Samuel Schuyler Sy <schuylersy@gmail.com>
2019-04-20 19:51:35 +0800

    Add Teacher Button is functional

--- grading/system/login.php
+++ grading/system/login.php
@@ -153,6 +153,33 @@ session_start();
         
       ?>
 
+      <?php
+  if(isset($_POST['btn_add_teacher'])){
+    $txt_fname = $_POST['txt_fname'];
+    $txt_mname = $_POST['txt_mname'];
+    $txt_lname = $_POST['txt_lname'];
+    $txt_contact = $_POST['txt_contact'];
+    $txt_addr = $_POST['txt_addr'];
+    $txt_uname = $_POST['txt_uname'];
+    $txt_pass = $_POST['txt_pass'];
+
+    $chk = mysqli_query($con,"SELECT * from tblteacher where lname = '".$txt_lname."' and fname = '".$txt_fname."' and mname = '".$txt_mname."' ");
+    $ct = mysqli_num_rows($chk);
+
+    if($ct == 0){
+      $query = mysqli_query($con,"INSERT INTO tblteacher (lname,fname,mname,contact,address,username,password) values ('".$txt_lname."','".$txt_fname."','".$txt_mname."','".$txt_contact."','".$txt_addr."','".$txt_uname."','".$txt_pass."')"); 
+      if($query == true){
+              $_SESSION['added'] = 1;
+              header ("location: ".$_SERVER['REQUEST_URI']);
+      }
+    }
+    else{
+      $_SESSION['duplicate'] = 1;
+            header ("location: ".$_SERVER['REQUEST_URI']);
+    }
+  }
+?>
+
 
 <?php 
  include "pages/connection.php"; 
